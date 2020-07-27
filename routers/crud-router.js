7a365e808048e7999707686bfd1ceadd15eb2f97
booldook <booldook@gmail.com>
2020-07-27 12:56:19 +0900

    crud - create 완성

--- routers/crud-router.js
+++ routers/crud-router.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const { pool } = require('../modules/mysql-mod');
 
 router.get(["/", "/li", "/li/:page"], (req, res, next) => { // 리스트를 보여주기/SELECT
-
+	res.send("리스트 페이지");
 });
 
 router.get("/wr", (req, res, next) => { // 신규 작성 폼 보여주기/PUG
@@ -19,8 +20,20 @@ router.get("/rev/:id", (req, res, next) => { // 삭제하기/DELETE
 
 });
 
-router.post("/save", (req, res, next) => { // 저장하기/INSERT
-	res.send("....");
+router.post("/save", async (req, res, next) => { // 저장하기/INSERT
+	let { name, age } = req.body;
+	let connect, sql, sqlVal, result;
+	try {
+		sql = 'INSERT INTO review SET name=?, age=?';
+		sqlVal = [name, age];
+		connect = await pool.getConnection();
+		result = await connect.execute(sql, sqlVal);
+		connect.release();
+		res.redirect('/crud/li');
+	}
+	catch(e) {
+		res.json(e);
+	}
 });
 
 module.exports = router;
